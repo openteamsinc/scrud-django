36efd3c1d5c8641ff8370db76681e531f05cc96f
David Charboneau <david@adadabase.com>
2020-08-23 21:54:05 -0400

    Add new decorators for scrudful handling.

--- scrud_django/serializers.py
+++ scrud_django/serializers.py
@@ -1,4 +1,7 @@
+from datetime import timezone
+
 from django.db.models import Manager
+from django.utils.http import http_date, quote_etag
 from rest_framework import serializers
 from rest_framework.reverse import reverse_lazy
 
@@ -23,14 +26,17 @@ class ResourceSerializer(serializers.Serializer):
         if not envelope:
             return instance.content
         request = self._context["request"]
+        last_modified = http_date(
+            instance.modified_at.replace(tzinfo=timezone.utc).timestamp()
+        )
         return {
             'href': reverse_lazy(
                 instance.resource_type.route_name_detail(),
                 args=[instance.id],
                 request=request,
             ),
-            'last_modified': instance.modified_at.isoformat(),
-            'etag': instance.etag,
+            'last_modified': last_modified,
+            'etag': quote_etag(instance.etag),
             'content': instance.content,
         }
 
--- scrud_django/views.py
+++ scrud_django/views.py
@@ -1,6 +1,10 @@
+from calendar import timegm
+from functools import wraps
 from typing import Optional
 
 from django.shortcuts import get_object_or_404
+from django.utils.cache import get_conditional_response
+from django.utils.http import quote_etag
 from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -10,9 +14,125 @@ from scrud_django.models import Resource, ResourceType
 from scrud_django.paginations import StandardResultsSetPagination
 from scrud_django.registration import ResourceRegistration
 
+
+def link_content(url, rel, content_type):
+    return f"<{url}>; rel=\"{rel}\"; type=\"{content_type}\""
+
+
+def get_string_or_evaluate(string_or_func, request, *args, **kwargs):
+    if not string_or_func:
+        return None
+    if isinstance(string_or_func, str):
+        return string_or_func
+    return string_or_func(request, *args, **kwargs)
+
+
+def scrudful(
+    etag_func=None,
+    last_modified_func=None,
+    schema_link_or_func=None,
+    context_link_or_func=None,
+    http_schema_or_func=None,
+):
+    """Decorator to make a view method SCRUDful"""
+    # TODO what about 400 Bad Request context and schema?
+    def decorator(view_method):
+        @wraps(view_method)
+        def inner(self, request, *args, **kwargs):
+            if request.method in ("PUT", "DELETE"):
+                if not request.META.get(
+                    "HTTP_IF_UNMODIFIED_SINCE"
+                ) or not request.META.get("HTTP_IF_MATCH"):
+                    # TODO Define standard error json
+                    response = Response(
+                        {
+                            "missing-required-headers": "If-Match and If-Unmodified-Since headers are required."  # noqa
+                        },
+                        status=400,
+                    )
+                    return response
+
+            # Compute values (if any) for the requested resource.
+            def get_last_modified():
+                if last_modified_func:
+                    last_modified = last_modified_func(
+                        request, *args, **kwargs
+                    )
+                    if last_modified:
+                        return timegm(last_modified.utctimetuple())
+                return None
+
+            etag = etag_func(request, *args, **kwargs) if etag_func else None
+            etag = quote_etag(etag) if etag else None
+            last_modified = get_last_modified()
+            response = get_conditional_response(
+                request, etag=etag, last_modified=last_modified
+            )
+            if response is None:
+                response = view_method(self, request, *args, **kwargs)
+                schema_link = (
+                    get_string_or_evaluate(
+                        schema_link_or_func, request, *args, **kwargs
+                    )
+                    or ""
+                )
+                context_link = (
+                    get_string_or_evaluate(
+                        context_link_or_func, request, *args, **kwargs
+                    )
+                    or ""
+                )
+                join_links = ", " if schema_link and context_link else ""
+                link_content = schema_link + join_links + context_link
+                if etag:
+                    response["ETag"] = etag
+                if last_modified:
+                    response["Last-Modified"] = last_modified
+                if link_content:
+                    response["Link"] = link_content
+            return response
+
+        setattr(inner, "http_schema_or_func", http_schema_or_func)
+        return inner
+
+    return decorator
+
+
+def scrudful_viewset(cls):
+    meta = getattr(cls, "Meta", None)
+    etag_func = getattr(meta, "etag_func", None)
+    last_modified_func = getattr(meta, "last_modified_func", None)
+    schema_link_or_func = getattr(meta, "schema_link_or_func", None)
+    context_link_or_func = getattr(meta, "context_link_or_func", None)
+    scrudful_item = scrudful(
+        etag_func=etag_func,
+        last_modified_func=last_modified_func,
+        schema_link_or_func=schema_link_or_func,
+        context_link_or_func=context_link_or_func,
+    )
+    for method_name in ("create", "retrieve", "update", "destroy"):
+        method = getattr(cls, method_name)
+        setattr(cls, method_name, scrudful_item(method))
+    if hasattr(cls, "list"):
+        scrudful_list = scrudful(
+            etag_func=getattr(meta, "list_etag_func", None),
+            last_modified_func=getattr(meta, "list_last_modified_func", None),
+            schema_link_or_func=getattr(
+                meta, "list_schema_link_or_func", None
+            ),
+            context_link_or_func=getattr(
+                meta, "list_context_link_or_func", None
+            ),
+        )
+        list_method = getattr(cls, "list")
+        setattr(cls, "list", scrudful_list(list_method))
+    return cls
+
+
 # RESOURCE
 
 
+@scrudful_viewset
 class ResourceViewSet(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
     serializer_class = serializers.ResourceSerializer
@@ -20,6 +140,21 @@ class ResourceViewSet(viewsets.ModelViewSet):
 
     # scrud variable
     resource_type_name: Optional[str] = None
+
+    class Meta:
+        def etag_func(request, *args, **kwargs):
+            return "XXXX"
+
+        # last_modified_func =
+        schema_link_or_func = "schema-tbd"
+        context_link_or_func = "context-tbd"
+
+        def list_etag_func(request, *args, **kwargs):
+            return "YYYY"
+
+        # list_last_modified_func =
+        list_schema_link_or_func = "list-schema-tbd"
+        list_context_link_or_func = "list-context-tbd"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
