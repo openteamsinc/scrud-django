1fe70b4f944accafa50dba34e37e5bc47ce76559
David Charboneau <david@adadabase.com>
2020-11-18 10:40:04 -0500

    bumped version to 0.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='scrud-django',
-    version='0.1',
+    version='0.2',
     description='A Django app for Semantic CRUD.',
     long_description=long_description,
     url='https://github.com/openteamsinc/scrud-django',
